ef3e106d39dae7501f2fdbedf42a77ab7b2478ea
LEEJEEHYUN <jhsanta@naver.com>
2015-10-30 00:59:54 +0900

    all complete

--- src/DeleteStuInfo.java
+++ src/DeleteStuInfo.java
@@ -1,42 +1,33 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList; 
-import java.util.List; 
 import java.util.StringTokenizer;
-
 
 public class DeleteStuInfo extends FileManage{
 	public void Deletestuinfo(int stu){
 		String id = Integer.toString(stu);
 		String strLine = "0"; 
 		String dummy = ""; 
-		List<Float> x = new ArrayList<Float>();  
 
 		try { 
 			BufferedReader fileReader = new BufferedReader(new FileReader(stu_info)); 
-			int cnt = 0; 
 			while ((strLine = fileReader.readLine()) != null) { 
-				System.out.println(strLine);
 				StringTokenizer st = new StringTokenizer(strLine,",");
 				String stu_id = st.nextToken(); 
 				if(!stu_id.equals(id)) { 
-					dummy += strLine + "\r\n"; 
-					System.out.println(dummy);
+					dummy += strLine + "\n"; 
 				}
-				strLine = fileReader.readLine();
-				cnt++; 
 			} 
 
 			FileWriter fw = new FileWriter(stu_info); 
 			fw.write(dummy); 
 			fw.flush(); 
 			fw.close(); 
-			fileReader.close(); 
-		} catch (IOException ioe) { 
+			fileReader.close();
+			System.out.println("======== 학생의 정보가 삭제되었습니다. ========");
+			System.out.println();
+		} catch (IOException ioe){ 
 			ioe.printStackTrace(); 
 		} 
 	}	
--- src/UpdateStuInfo.java
+++ src/UpdateStuInfo.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
@@ -15,8 +16,35 @@ public class UpdateStuInfo extends FileManage{
 		Scanner scan = new Scanner(System.in);
 		System.out.print("수정할 핸드폰 번호를 입력하세요 : ");
 		String phonenum = scan.nextLine();
-		
+		System.out.println();
 		String id = Integer.toString(stu);
+		String strLine = "0"; 
+		String dummy = ""; 
+
+		try { 
+			BufferedReader fileReader = new BufferedReader(new FileReader(stu_info)); 
+			while ((strLine = fileReader.readLine()) != null) { 
+				StringTokenizer st = new StringTokenizer(strLine,",");
+				String stu_id = st.nextToken(); 
+				if(stu_id.equals(id)) { 
+					String stu_name = st.nextToken();
+					String stu_dept = st.nextToken();
+					dummy += stu_id + "," + stu_name + "," + stu_dept + "," + phonenum + "\n";
+				}
+				else
+					dummy += strLine + "\n"; 
+			} 
+
+			FileWriter fw = new FileWriter(stu_info); 
+			fw.write(dummy); 
+			fw.flush(); 
+			fw.close(); 
+			fileReader.close();
+			System.out.println("======== 학생의 정보가 변경되었습니다. ========");
+			System.out.println();
+		} catch (IOException ioe){ 
+			ioe.printStackTrace(); 
+		} 
 	}
 }
 
